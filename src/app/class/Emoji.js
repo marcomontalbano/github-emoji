cf9583ac825205267dbff6ae8518ffeabdcc5923
Marco Montalbano <marcomontalbano.work@gmail.com>
2018-04-29 11:19:35 +0200

    Changed emoji.json structure.

--- src/app/class/Emoji.js
+++ src/app/class/Emoji.js
@@ -1,12 +1,10 @@
 
 export default class Emoji {
 
-    constructor({ code, html_entity, image, image_unicode, image_best, name, unicode }) {
+    constructor({ code, html_entity, image, name, unicode }) {
         this.code = code;
         this.html_entity = html_entity;
         this.image = image;
-        this.image_unicode = image_unicode;
-        this.image_best = image_best;
         this.name = name;
         this.unicode = unicode;
     }
