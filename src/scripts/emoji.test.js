168b8371d01b78ba4be120fdede534e3b1dba89c
Marco Montalbano <marcomontalbano.work@gmail.com>
2021-12-21 21:25:14 +0100

    Update all dependencies

--- src/scripts/emoji.test.js
+++ src/scripts/emoji.test.js
@@ -7,6 +7,6 @@ const { dumpEmoji } = require('./emoji');
 describe('dump-emoji', () => {
     it('should contains the correct number of emojis', async () => {
         const emojis = await dumpEmoji();
-        expect(Object.keys(emojis).length).toBe(1799);
+        expect(Object.keys(emojis).length).toBe(1874);
     })
 })
